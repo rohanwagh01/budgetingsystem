5270ccaba53d302cd7c42b1c7d3241991fa417a1
Rohan <rwagh@mit.edu>
2022-11-07 15:54:48 -0500

    introduced barebones for database and storage of user info

--- website/__init__.py
+++ website/__init__.py
@@ -10,8 +10,11 @@ def create_app():
     Create a flask application
     '''
     app = Flask(__name__)
-    #secure cookies and session data, aware that this is a goofy way to store this
+    #secure cookies and session data, aware that this is a goofy way to store this fix in scaling
     app.config['SECRET_KEY'] = 'T[;4eT%}!E:gl1X'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app) #connect database to the flask app, fire, damn, thats cool, now make tables for user info and budgeting
+
 
     from .views import views
     from .auth import auth
